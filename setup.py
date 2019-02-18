f7cf259fe139029f632ed62dbc2925efee126da4
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2019-02-18 13:19:20 -0500

    added skeleton vizserver.py, some external dependencies

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     install_requires=INSTALL_REQUIRES,
     entry_points={
         'console_scripts':[
-            # 'vizserver=vizinspect.frontend.indexserver:main',
+            'vizserver=vizinspect.frontend.vizserver:main',
             # 'authnzerver=lccserver.authnzerver.main:main',
         ],
     },
---
+++ vizinspect/utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+'''utils.py - Waqas Bhatti (wbhatti@astro.princeton.edu) - Aug 2018
+License: MIT - see the LICENSE file for the full text.
+
+This contains various utility functions and classes for lcc-server.
+
+'''
+
+# Use our custom ProcessPoolExecutor. This comes with a built-in finalizer
+# function in addition to the initializer and initargs kwargs added in the
+# Python 3.7 version. Should work in Python < 3.7 as well.
+from vizserver.external.futures37.process import ProcessPoolExecutor
+ProcExecutor = ProcessPoolExecutor
