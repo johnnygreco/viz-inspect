287754b34ee8cd26cbf045d34a1abdad51fdd40a
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2019-03-07 15:48:22 -0500

    backend.images: add plotting code

--- vizinspect/backend/images.py
+++ vizinspect/backend/images.py
@@ -53,7 +53,7 @@ from matplotlib.figure import Figure
 ## LOADING GALAXY IMAGES ##
 ###########################
 
-def load_galaxy_image(object_index, basedir, source_subdir='images'):
+def load_galaxy_image(object_index, basedir, images_subdir='images'):
     '''This loads a Galaxy image from a PNG into an array readable by matplotlib.
 
     Parameters
@@ -65,7 +65,7 @@ def load_galaxy_image(object_index, basedir, source_subdir='images'):
     basedir : str
         The base directory that the viz-inspect server is working in.
 
-    source_subdir : str
+    images_subdir : str
         The subdirectory under the `basedir` containing all of the images.
 
     Returns
@@ -78,7 +78,7 @@ def load_galaxy_image(object_index, basedir, source_subdir='images'):
     '''
 
     image_fpath = os.path.join(basedir,
-                               source_subdir,
+                               images_subdir,
                                'candy-{}.png'.format(object_index))
 
     try:
@@ -94,3 +94,109 @@ def load_galaxy_image(object_index, basedir, source_subdir='images'):
 ##################
 ## MAKING PLOTS ##
 ##################
+
+def make_main_plot(catalog,
+                   source_index,
+                   basedir,
+                   plot_fontsize=15,
+                   images_subdir='images',
+                   site_datadir='viz-inspect-images',
+                   outfile=None):
+    '''This generates the main plot.
+
+    Parameters
+    ----------
+
+    catalog : pandas.DataFrame
+        This is the catalog of objects loaded into a pandas dataframe.
+
+    source_index : int
+        The index of the object to make a plot for.
+
+    basedir : str
+        The base directory where the viz-inspect server is operating.
+
+    plot_fontsize: int
+        The font-size of the plot to make in points.
+
+    images_subdir : str
+        The subdirectory under the `basedir` where the object images are
+        located.
+
+    site_datadir : str
+        The subdirectory under the `basedir` where the images to serve to the
+        frontend will be placed.
+
+    outfile : str or None
+        The output PNG file name where the plot will be written to. If this is
+        None, the plot will be written to a file called
+        'current-object-plot.png' in the site-static directory so the
+        viz-inspect server can load it into the interface. If this is a str,
+        should be a path indicating where the output plot file will be written.
+
+    Returns
+    -------
+
+    str
+        The path where the plot was written to.
+
+    '''
+
+    plot_fontsize = 15
+    fig = Figure(figsize=(10, 6))
+    adjust = dict(wspace=0.13,
+                  hspace=0.25,
+                  bottom=0.1,
+                  top=0.97,
+                  right=0.96,
+                  left=-0.02)
+    grid = plt.GridSpec(2, 3, **adjust)
+    ax_img = fig.add_subplot(
+        grid[0:2, 0:2], xticks=[], yticks=[])
+    ax_top = fig.add_subplot(grid[0,2])
+    ax_bot = fig.add_subplot(grid[1,2])
+
+    # add in the image of the object
+    img = load_galaxy_image(source_index,
+                            basedir,
+                            images_subdir=images_subdir)
+
+    # FIXME: check if the image's origin really is 0,0 in the bottom-left. If
+    # not, can remove origin kwarg below.
+    ax_img.imshow(img,origin='lower')
+
+    ax_top.scatter(
+        catalog['g-i'],
+        catalog['g-r'],
+        alpha=0.3
+    )
+    ax_top.set_xlabel('$g-i$', fontsize=plot_fontsize)
+    ax_top.set_ylabel('$g-r$', fontsize=plot_fontsize)
+    ax_top.set_xlim(catalog['g-i'].min()-0.1,
+                    catalog['g-i'].max()+0.1)
+    ax_top.set_ylim(catalog['g-r'].min()-0.1,
+                    catalog['g-r'].max()+0.1)
+
+    ax_bot.scatter(
+        catalog['r_e'],
+        catalog.mu_e_ave_forced_g,
+        alpha=0.3
+    )
+    ax_bot.set_xlabel(
+        r'$r_\mathrm{eff}\ \mathrm{[arcsec]}$',
+        fontsize=plot_fontsize)
+    ax_bot.set_ylabel(
+        r'$\langle\mu_e(g)\rangle\ \mathrm{[mag/arcsec^2]}$',
+        fontsize=plot_fontsize)
+    ax_bot.set_xlim(0, 16)
+    ax_bot.set_ylim(24, 29)
+
+    if outfile is None:
+
+        outfile = os.path.join(basedir,
+                               site_datadir,
+                               'current-object-plot.png')
+
+    fig.savefig(outfile,dpi=100)
+
+    return outfile
