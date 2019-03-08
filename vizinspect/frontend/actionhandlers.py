7325c312f1ff1f099edd22ac9fe10067ca854978
Waqas Bhatti <waqas.afzal.bhatti@gmail.com>
2019-03-07 23:28:58 -0500

    WIP: UI fixes, added /api/load-object handler

--- vizinspect/frontend/actionhandlers.py
+++ vizinspect/frontend/actionhandlers.py
@@ -15,6 +15,8 @@ import logging
 import numpy as np
 from datetime import datetime
 
+import os.path
+
 # for generating encrypted token information
 from cryptography.fernet import Fernet
 
@@ -70,6 +72,7 @@ LOGGER = logging.getLogger(__name__)
 
 from tornado import gen
 from tornado.httpclient import AsyncHTTPClient
+from tornado.escape import xhtml_escape
 
 
 ###################
@@ -77,6 +80,98 @@ from tornado.httpclient import AsyncHTTPClient
 ###################
 
 from .basehandler import BaseHandler
+
+from ..backend import catalogs, images
+
+
+######################
+## WORKER FUNCTIONS ##
+######################
+
+def _load_object_from_catalog(
+        catalog_csv,
+        source_index,
+        basedir,
+        review_mode=False,
+        flags_to_use=('candy','junk','tidal','cirrus'),
+        load_comments=True,
+        plot_fontsize=15,
+        images_subdir='images',
+        site_datadir='viz-inspect-data',
+):
+    '''
+    This does the actual work of loading the object.
+
+    Runs in an executor.
+
+    - gets the object from the catalog CSV
+    - gets the object's comments from the comments CSV
+    - gets the object's plot. writes plot to the site_datadir
+    - plot files have names like '{catalog_fname}-source-{index}-plot.png'
+    - these are kept around until the server exits
+    - if a plot file exists, we don't try to make a new one
+
+    '''
+
+    cat, comm = catalogs.load_catalog(catalog_csv,
+                                      review_mode=review_mode,
+                                      flags_to_use=flags_to_use,
+                                      load_comments=load_comments)
+
+    if source_index > len(cat):
+        LOGGER.error(
+            'source index requested: %s is > len(catalog) = %s' % (
+                source_index,
+                len(cat)
+            )
+        )
+        return None
+
+    #
+    # get the plot for the object
+    #
+
+    plotfile = os.path.join(
+        basedir,
+        images_subdir,
+        '{catalog_fname}-{index}-plot.png'.format(
+            catalog_fname=os.path.splitext(os.path.basename(catalog_csv))[0],
+            index=source_index
+        )
+    )
+
+    if not os.path.exists(plotfile):
+
+        try:
+            images.make_main_plot(
+                cat,
+                source_index,
+                basedir,
+                plot_fontsize=plot_fontsize,
+                images_subdir=images_subdir,
+                site_datadir=site_datadir,
+                outfile=plotfile
+            )
+
+        except Exception as e:
+            LOGGER.exception('could not make plot for '
+                             'source: %s in catalog: %s'
+                             % (source_index, catalog_csv))
+            plotfile = None
+
+    #
+    # get the comments for the object
+    #
+    object_comments = comm[comm['source_index'] == source_index]
+
+    # this is the dict we return
+    retdict = {
+        'info': cat.iloc[source_index],
+        'plot':plotfile,
+        'comments':object_comments
+    }
+
+    return retdict
 
 
 
@@ -102,7 +197,8 @@ class LoadObjectHandler(BaseHandler):
                    ratelimit,
                    cachedir,
                    catalog_csv,
-                   comments_csv):
+                   flags_to_use,
+                   file_lock):
         '''
         handles initial setup.
 
@@ -123,7 +219,11 @@ class LoadObjectHandler(BaseHandler):
         self.cachedir = cachedir
 
         self.catalog_csv = catalog_csv
-        self.comments_csv = comments_csv
+        self.flags_to_use = flags_to_use
+
+        # this is a tornado.locks Lock to serialize access to the CSV files on
+        # disk
+        self.file_lock = file_lock
 
 
     @gen.coroutine
@@ -133,17 +233,43 @@ class LoadObjectHandler(BaseHandler):
         Gets catalog and comment info, plots the object if not already plotted,
         and then returns JSON with everything.
 
-
         '''
 
-        self.render(
-            'index.html',
-            flash_messages=self.render_flash_messages(),
-            user_account_box=self.render_user_account_box(),
-            page_title='viz-inspect',
-            siteinfo=self.siteinfo,
-            current_user=self.current_user,
-        )
+        try:
+
+            objindex = int(xhtml_escape(source_index))
+            if objindex < 0:
+                objindex = 0
+
+            objectinfo = yield self.executor.submit(
+                _load_object_from_catalog,
+                self.catalog_csv,
+                source_index,
+                self.basedir,
+                review_mode=False,
+                flags_to_use=self.flags_to_use,
+                load_comments=True,
+                plot_fontsize=15,
+                images_subdir=self.siteinfo['images_subdir'],
+                site_datadir=self.siteinfo['data_path']
+            )
+
+            retdict = {'status':'ok',
+                       'message':'object found OK',
+                       'result':objectinfo}
+
+            self.write(retdict)
+            self.finish()
+
+        except Exception as e:
+
+            LOGGER.exception('failed to get requested source_index')
+            self.set_status(400)
+            retdict = {'status':'failed',
+                       'message':'Invalid request for object index.',
+                       'result':None}
+            self.write(retdict)
+            self.finish()
 
 
 
@@ -165,7 +291,8 @@ class SaveObjectHandler(BaseHandler):
                    ratelimit,
                    cachedir,
                    catalog_csv,
-                   comment_csv):
+                   comment_csv,
+                   file_lock):
         '''
         handles initial setup.
 
@@ -187,6 +314,10 @@ class SaveObjectHandler(BaseHandler):
 
         self.catalog_csv = catalog_csv
         self.comment_csv = comment_csv
+
+        # this is a tornado.locks Lock to serialize access to the CSV files on
+        # disk
+        self.file_lock = file_lock
 
 
     @gen.coroutine
